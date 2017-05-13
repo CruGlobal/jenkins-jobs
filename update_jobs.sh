0a49c019fa61bef03c2e613db214ec0690ff7965
Harro <michael.harrison@cru.org>
2017-05-13 13:54:53 -0400

    adds file for mobile jobs

--- update_jobs.sh
+++ update_jobs.sh
@@ -2,4 +2,5 @@
 
 cd `dirname $0`
 jenkins-jobs --conf jenkins_jobs.ini update jobs/jobs.yml $@
+jenkins-jobs --conf jenkins_jobs.ini update jobs/mobile-jobs.yml $@
 python update_promotion_process.py $@
