f62a943fc78659cc5a44d93a0f526203524bca7a
Matt Drees <matt.drees@cru.org>
2018-06-22 13:35:03 -0600

    remove slackSend
    We don't use slack.

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -9,7 +9,6 @@ def call(String buildStatus = 'STARTED') {
 
     // Default values
     def color = 'RED'
-    def colorCode = '#FF0000'
     def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
     def summary = "${subject} (${env.BUILD_URL})"
     def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
@@ -18,15 +17,11 @@ def call(String buildStatus = 'STARTED') {
     // Override default values based on build status
     if (buildStatus == 'STARTED') {
         color = 'YELLOW'
-        colorCode = '#FFFF00'
     } else if (buildStatus == 'SUCCESSFUL') {
         color = 'GREEN'
-        colorCode = '#00FF00'
     }
 
     // Send notifications
-    slackSend (color: colorCode, message: summary)
-
     hipchatSend (color: color, notify: true, message: summary)
 
     emailext (
