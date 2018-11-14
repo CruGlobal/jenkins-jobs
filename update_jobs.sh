e8691a79bda79847d8376501d9ab42680a2e61d4
Matt Drees <matt.drees@cru.org>
2018-11-14 15:09:00 -0700

    Add airflow-jobs to update_jobs.sh

--- update_jobs.sh
+++ update_jobs.sh
@@ -8,4 +8,5 @@ jenkins-jobs --conf jenkins_jobs.ini update jobs/java-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/ep-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/ep-promotable-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/pipeline-jobs.yml $@
+jenkins-jobs --conf jenkins_jobs.ini update jobs/airflow-jobs.yml $@
 python update_promotable_jobs.py $@
