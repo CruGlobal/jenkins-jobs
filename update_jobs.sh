54bc69c4472c57872d21191b22313bd80821cc67
Luis Rodriguez <luis.rodriguez@cru.org>
2019-06-28 10:35:45 -0400

    add job definition to update_jobs.sh

--- update_jobs.sh
+++ update_jobs.sh
@@ -10,4 +10,5 @@ jenkins-jobs --conf jenkins_jobs.ini update jobs/ep-promotable-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/pipeline-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/airflow-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/crustore2-jobs.yml $@
+jenkins-jobs --conf jenkins_jobs.ini update jobs/aem-dispatcher-jobs.yml $@
 python update_promotable_jobs.py $@
