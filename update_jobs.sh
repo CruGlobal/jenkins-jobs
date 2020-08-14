133c978e0bee837c0a3803c8667ed2857f623fb5
Brian Zoetewey <brian.zoetewey@cru.org>
2020-08-14 15:17:21 -0400

    Cleanup remaining unused jobs.

--- update_jobs.sh
+++ update_jobs.sh
@@ -4,7 +4,6 @@ cd `dirname $0`
 jenkins-jobs --conf jenkins_jobs.ini update jobs/simple-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/app-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/java-docker-jobs.yml $@
-jenkins-jobs --conf jenkins_jobs.ini update jobs/java-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/ep-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/ep-promotable-jobs.yml $@
 jenkins-jobs --conf jenkins_jobs.ini update jobs/pipeline-jobs.yml $@
